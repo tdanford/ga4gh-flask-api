600ca0f3e0fc4aff2320319687e4a74d39f97e71
Timothy Danford <tdanford@gmail.com>
2015-03-06 08:24:10 -0500

    Adding variant and variant-set search stubs.

--- ga4gh_service.py
+++ ga4gh_service.py
@@ -11,9 +11,17 @@ from json_models import *
 app = Flask(__name__)
 
 @app.route('/callsets/search', methods=['POST'])
-def hello_world():
+def search_callsets():
     callsets_request = GASearchCallSetsRequest_from_json(request.get_json())
     return callsets_response()
+
+@app.route('/variants/search', methods=['POST'])
+def search_variants():
+    pass
+
+@app.route('/variantsets/search', methods=['POST'])
+def search_variant_sets():
+    pass
 
 if __name__ == '__main__':
     app.run()
